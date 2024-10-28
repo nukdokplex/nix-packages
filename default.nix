c15cc2f11b9dcf8068d5669b0507093802b409ac
nukdokplex <nukdokplex@nukdokplex.ru>
2024-10-28 19:20:42 +0500

    add epson_201310w and initial changes

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  epson_201310w = pkgs.callPackage ./pkgs/epson_201310w { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
